978feea2fb1734b99752447b1a496adba92bd522
Erick Sena <ericksena621@gmail.com>
2023-10-21 13:33:31 -0300

    feat: add content

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,30 @@
+export const dataFake = [
+  {
+    id: '1',
+    photoCover:
+      'https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png',
+    title: 'Notícia Principal',
+    description: 'Descrição da notícia principal',
+  },
+  {
+    id: '2',
+    photoCover:
+      'https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png',
+    title: 'Noticia Secundária - 1',
+    description: 'Descrição da secundária - 1',
+  },
+  {
+    id: '3',
+    photoCover:
+      'https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png',
+    title: 'Noticia Secundária - 2',
+    description: 'Descrição da secundária - 2',
+  },
+  {
+    id: '4',
+    photoCover:
+      'https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png',
+    title: 'Noticia Secundária - 3',
+    description: 'Descrição da secundária - 3',
+  },
+];
